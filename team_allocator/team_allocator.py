c89c178f58cce5cad8ea0e39108abaa6ab31c522
gianmillare <gianmillaregsm@gmail.com>
2020-02-16 23:59:22 -0800

    formulating and printing out team 1 players

--- team_allocator/team_allocator.py
+++ team_allocator/team_allocator.py
@@ -16,3 +16,8 @@ random.shuffle(players)
 team1 = players[:len(players)//2]
 
 print("The captain for Team 1 is: " + random.choice(team1))
+
+print("Team 1: ")
+
+for player in team1:
+    print(player)
