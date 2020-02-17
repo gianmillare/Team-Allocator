d925ece068088819f65dbb4293e6b8fe6921b225
gianmillare <gianmillaregsm@gmail.com>
2020-02-17 00:10:24 -0800

    created an input function to rerun the code if user outputs undesired team

--- team_allocator/team_allocator.py
+++ team_allocator/team_allocator.py
@@ -32,3 +32,8 @@ while True:
 
     for player in team2:
         print(player)
+
+response = input("Would you like to pick teams again? Type 'y' for YES and 'n' for NO: ")
+
+if response --"n":
+    break
