0ca2e002d798b831a05aff8c44cb47365fe071ba
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2020-02-17 08:08:38 -0800

    created new folder for team allocator multiple teams

---
+++ team_allocator2/team_allocator2.py
@@ -0,0 +1,11 @@
+import random
+
+print("Welcome to Team Allocator! This version contains up to 3 teams!")
+
+
+players = ["Craig", "Ben", "Serena", "Amanda",
+           "Henry", "Groot", "Gamora", "Widow",
+           "Steve", "Tony", "Rocket", "Wasp",
+           "Peter", "Bucky", "Pepper", "Alina",
+           "Michael", "Dwight", "Pam", "Stanley",
+           "Jim", "Janice", "Sandra", "Veronica"]
