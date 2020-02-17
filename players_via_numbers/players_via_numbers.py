c836d207d4f7e58864233a0bc71e35581a62e233
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2020-02-17 09:45:30 -0800

    completed the Team allocator for number based teams

--- players_via_numbers/players_via_numbers.py
+++ players_via_numbers/players_via_numbers.py
@@ -11,10 +11,21 @@ for i in range(1, number_of_players + 1):
 
 random.shuffle(players)
 
-team1 = players[:len(players)//2]
+while True:
+    team1 = players[:len(players)//2]
 
-print("\nThe captain for Team 1 is: " + str(random.choice(team1)))
-print("\nThe roster for Team 1 is: ")
-for player in team1:
-     print(player)
+    print("\nThe captain for Team 1 is: " + str(random.choice(team1)))
+    print("\nThe roster for Team 1 is: ")
+    for player in team1:
+         print(player)
     
+    team2 = players[len(players)//2:]
+
+    print("\nThe captain for Team 2 is: " +str(random.choice(team2)))
+    print("\nThe roster for Team 2 is: ")
+    for player in team2:
+        print(player)
+
+    response = input("Would you like to run the Team Allocator again? Type 'y' for YES or 'n' for NO. ")
+    if response == "n":
+        break
