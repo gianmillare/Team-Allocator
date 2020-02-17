1be083b155c2105caaf54f9969cab0705d4900e9
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2020-02-17 08:16:42 -0800

    creating the allocator for two teams, revising for better code

--- team_allocator2/team_allocator2.py
+++ team_allocator2/team_allocator2.py
@@ -9,3 +9,23 @@ players = ["Craig", "Ben", "Serena", "Amanda",
            "Peter", "Bucky", "Pepper", "Alina",
            "Michael", "Dwight", "Pam", "Stanley",
            "Jim", "Janice", "Sandra", "Veronica"]
+
+while True:
+    random.shuffle(players)
+
+    team1 = players[:len(players)//3]
+    print("\nThe captain for Team 1 is: " + random.choice(team1))
+    print("\nThe roster for Team 1 is: ")
+    for player in team1:
+        print(player)
+
+    team2 = players[len(players)//3:(len(players)//3)*2]
+    print("\nThe captain for Team 2 is: " + random.choice(team2))
+    print("\nThe roster for Team 2 is: ")
+    for player in team2:
+        print(player)
+
+    response = input("Would you like to pick teams again? Type 'y' for YES or 'n' for NO: ")
+
+    if response == "n":
+        break
