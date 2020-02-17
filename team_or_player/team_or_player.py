0d63976568e77b209319743ec165d48e410440ef
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2020-02-17 08:37:27 -0800

    created another folder, new file allows user to choose between teams or players

---
+++ team_or_player/team_or_player.py
@@ -0,0 +1,8 @@
+import random
+
+players = ["Craig", "Ben", "Serena", "Amanda",
+           "Henry", "Groot", "Gamora", "Widow",
+           "Steve", "Tony", "Rocket", "Wasp",
+           "Peter", "Bucky", "Pepper", "Alina",
+           "Michael", "Dwight", "Pam", "Stanley",
+           "Jim", "Janice", "Sandra", "Veronica"]
