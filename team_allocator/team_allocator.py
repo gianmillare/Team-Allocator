d3d79bef84540285012a607fd933e755383aa3a0
gianmillare <gianmillaregsm@gmail.com>
2020-02-16 23:57:02 -0800

    pushing in code for shuffling and choice under the random library

---
+++ team_allocator/team_allocator.py
@@ -0,0 +1,18 @@
+import random
+
+print("Welcome to Team Allocator!")
+
+players = ["Geoff", "David", "Alayah", "Natasha",
+           "Craig", "Ben", "Serena", "Amanda",
+           "Henry", "Groot", "Gamora", "Widow",
+           "Steve", "Tony", "Rocket", "Wasp",
+           "Peter", "Bucky", "Pepper", "Alina",
+           "Michael", "Dwight", "Pam", "Stanley",
+           "Jim", "Janice", "Sandra", "Veronica"]
+
+
+random.shuffle(players)
+
+team1 = players[:len(players)//2]
+
+print("The captain for Team 1 is: " + random.choice(team1))
